24d6ff812a3b145d7a5c1a055d7b723cb4ebaa09
YU <111847207+YU000jp@users.noreply.github.com>
2024-04-06 15:48:52 +0900

    Merge branch 'whiteboard-emptySpace'

--- src/main/frontend/components/page.cljs
+++ src/main/frontend/components/page.cljs
@@ -486,7 +486,7 @@
              (when-not whiteboard?
                [:div.ls-page-title.flex-1.flex-row.w-full
                 (page-title page-name icon title format fmt-journal?)])
-             (when (not config/publishing?)
+             (when (and (not config/publishing?) (not whiteboard?))
                (when config/lsp-enabled?
                  [:div.flex.flex-row
                   (plugins/hook-ui-slot :page-head-actions-slotted nil)
@@ -507,11 +507,11 @@
                      (route-handler/redirect-to-page! @*current-block-page))]
              (page-blocks-cp repo page {:sidebar? sidebar? :whiteboard? whiteboard?}))]])
 
-       (when today?
-         (today-queries repo today? sidebar?))
-
-       (when today?
-         (scheduled/scheduled-and-deadlines page-name))
+       (when-not whiteboard?
+         (when today?
+           (today-queries repo today? sidebar?))
+         (when today?
+           (scheduled/scheduled-and-deadlines page-name)))
 
        (when-not block?
          (tagged-pages repo page-name))
