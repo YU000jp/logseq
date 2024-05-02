7a09d6c9469a110343b1a57095bd8ddbd6d0decc
YU <111847207+YU000jp@users.noreply.github.com>
2024-05-03 06:15:03 +0900

    履歴はページ名だけにする(バグ解消) / ブックマークに含まれるものはページ履歴に追加しない

--- src/main/frontend/handler/recent.cljs
+++ src/main/frontend/handler/recent.cljs
@@ -1,7 +1,9 @@
 (ns frontend.handler.recent
   "Fns related to recent pages feature"
   (:require [frontend.db :as db]
-            [frontend.db.model :as model]))
+            [frontend.db.model :as model]
+            [frontend.state :as state]
+            [frontend.util :as util]))
 
 (defn add-page-to-recent!
   [repo page-name-or-block-uuid click-from-recent?]
@@ -10,10 +12,11 @@
         page-name (if (uuid? page-name-or-block-uuid)
                     (when-let [block (model/get-block-by-uuid page-name-or-block-uuid)]
                       (get-in block [:block/page :block/original-name]))
-                    page-name-or-block-uuid)]
-
-    (when (or (and click-from-recent? (not ((set pages) page-name)))
-              (not click-from-recent?))
+                    page-name-or-block-uuid)
+        favorites (:favorites (state/sub-config))
+        favorited? (contains? (set (map util/page-name-sanity-lc favorites))
+                              page-name)] 
+    (when (and (not click-from-recent?) (not ((set pages) page-name)) (not favorited?));;fix: ブックマークに含まれるものは履歴に入れない
       (let [new-pages (take 20 (distinct (cons page-name pages)))]
         (db/set-key-value repo :recent/pages new-pages)))))
 
