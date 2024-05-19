7cd94d4f29f0afcd83ac0b5a3f608a1060c195cb
YU <111847207+YU000jp@users.noreply.github.com>
2024-05-19 19:22:52 +0900

    Dev: UIサンプル を開く 追加

--- src/main/frontend/components/header.cljs
+++ src/main/frontend/components/header.cljs
@@ -133,6 +133,12 @@
         :title (t :right-side-bar/all-files)
         :options {:href (rfe/href :all-files)}
         :icon (ui/icon "files")})
+       
+       (when (and config/dev? (state/sub [:ui/developer-mode?]))
+       {:class "ui"
+        :title "Dev: UI"
+        :options {:href (rfe/href :ui)}
+        :icon (ui/icon "plug")})
 
       ;;  {:title (t :help/shortcuts)
       ;;     :options {:on-click #(state/sidebar-add-block! (state/get-current-repo) "shortcut-settings" :shortcut-settings)} ;; :on-click #(state/pub-event! [:modal/keymap])
