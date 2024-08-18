a2e5ccba5c69eaa0c1ac094405ce1cdb6b81a391
YU <111847207+YU000jp@users.noreply.github.com>
2024-08-18 09:16:48 +0900

    SCHEDULED AND DEADLINE fix bug??

--- src/main/frontend/components/scheduled_deadlines.cljs
+++ src/main/frontend/components/scheduled_deadlines.cljs
@@ -14,48 +14,49 @@
 (defn- scheduled-or-deadlines?
   [page-name]
   (and (date/valid-journal-title? (string/capitalize page-name))
-       (not (true? (state/scheduled-deadlines-disabled?)))
-       (= (string/lower-case page-name) (string/lower-case (date/journal-name)))))
+       (not (true? (state/scheduled-deadlines-disabled?)))))
 
-(rum/defc  get-refs < db-mixins/query
+
+(defn- get-refs
   [page-name]
-  (when (scheduled-or-deadlines? page-name)
-    (db/get-date-scheduled-or-deadlines (string/capitalize page-name))))
+  (when (true? (scheduled-or-deadlines? page-name))
+    (db/get-scheduled-or-deadlines (string/capitalize page-name))))
 
 (rum/defc scheduled-and-deadlines-inner
   [page-name refs]
-  (if refs
-    [:div.scheduled-deadlines.references-blocks.mb-2.text-sm
-     (let [ref-hiccup (block/->hiccup refs
-                                      {:id (str page-name "-agenda")
-                                       :ref? true
-                                       :group-by-page? true
-                                       :editor-box editor/box}
-                                      {})]
-       (content/content page-name {:hiccup ref-hiccup}))]
-    [:span (t :right-side-bar/scheduled-and-deadline-no-result)]))
+  (when refs
+    (if (seq refs)
+      [:div.scheduled-deadlines.references-blocks.mb-2.text-sm
+       (let [ref-hiccup (block/->hiccup refs
+                                        {:id (str page-name "-agenda")
+                                         :ref? true
+                                         :group-by-page? true
+                                         :editor-box editor/box}
+                                        {})]
+         (content/content page-name {:hiccup ref-hiccup}))]
+      [:span (t :right-side-bar/scheduled-and-deadline-no-result)])))
 
 (rum/defc scheduled-and-deadlines
   [page-name]
-  (when-let [refs (get-refs page-name)]
+  (let [refs (get-refs page-name)]
     (scheduled-and-deadlines-inner page-name refs)))
 
+(rum/defc scheduled-and-deadlines-for-left-menu < rum/reactive db-mixins/query
+  [page-name on-contents-scroll]
+  (let [refs (get-refs page-name)]
+    (when (seq refs)
+      [:div.nav-contents-container.gap-1.pt-1
+       {:on-scroll on-contents-scroll}
+       [:details
+        {:open "true"}
+        [:summary
+         [(ui/icon "calendar-time" {:class "text-sm mr-1"})
 
-;;  FIXME: メニューバーではなぜか動作しない (値が存在しないとのエラーが出る)
-;; (rum/defc scheduled-and-deadlines-for-left-menu < rum/reactive
-;;   [page-name on-contents-scroll]
-;;   (when-let [refs (get-refs page-name)]
-;;     [:div.nav-contents-container.gap-1.pt-1
-;;      {:on-scroll on-contents-scroll}
-;;      [:details
-;;       {:open "true"}
-;;       [:summary
-;;        [(ui/icon "calendar-time" {:class "text-sm mr-1"})
-
-;;         (let [countNumber (count refs)]
-;;           [:span.overflow-hidden.text-ellipsis
-;;            {:title (t :right-side-bar/scheduled-and-deadline-desc)}
-;;            [(t :right-side-bar/scheduled-and-deadline) " (" countNumber ")"]])]]
-;;       (scheduled-and-deadlines-inner page-name refs)]]))
+          (let [countNumber (count refs)]
+            [:span.overflow-hidden.text-ellipsis
+             {:title (t :right-side-bar/scheduled-and-deadline-desc)}
+             [(t :right-side-bar/scheduled-and-deadline) " (" countNumber ")"]])]]
+        (scheduled-and-deadlines-inner page-name refs)]])))
 
 ;;TODO: Logseqアプリを開いたときに、SCHEDULEDとDEADLINEがあったら、ユーザーに通知する
+
